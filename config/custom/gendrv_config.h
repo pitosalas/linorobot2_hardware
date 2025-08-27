ad6aebeecd869cbc10df7f7323e87a44ffa5950e
Pito Salas <pitosalas@gmail.com>
2025-08-27 18:45:53 -0400

    small changes still tuning

--- config/custom/gendrv_config.h
+++ config/custom/gendrv_config.h
@@ -66,7 +66,7 @@ ROBOT ORIENTATION
 
 //define your robot' specs here
 #define MOTOR_MAX_RPM 253                   // motor's max RPM
-#define MAX_RPM_RATIO 0.60                  // MAX_RPM_ALLOWED = MOTOR_MAX_RPM * MAX_RPM_RATIO
+#define MAX_RPM_RATIO 0.8                  // MAX_RPM_ALLOWED = MOTOR_MAX_RPM * MAX_RPM_RATIO
 #define MOTOR_OPERATING_VOLTAGE 12          // motor's operating voltage (used to calculate max RPM)
 #define MOTOR_POWER_MAX_VOLTAGE 12          // max voltage of the motor's power source (used to calculate max RPM)
 #define MOTOR_POWER_MEASURED_VOLTAGE 12     // current voltage reading of the power connected to the motor (used for calibration)
@@ -75,7 +75,7 @@ ROBOT ORIENTATION
 #define COUNTS_PER_REV3 450                 // wheel3 encoder's no of ticks per rev
 #define COUNTS_PER_REV4 450                 // wheel4 encoder's no of ticks per rev
 #define WHEEL_DIAMETER 0.063                // wheel's diameter in meters
-#define LR_WHEELS_DISTANCE 0.202            // distance between left and right wheels
+#define LR_WHEELS_DISTANCE 0.215            // distance between left and right wheels
 #define PWM_BITS 10                         // PWM Resolution of the microcontroller
 #define PWM_FREQUENCY 20000                 // PWM Frequency
 #define SERVO_BITS 12                       // Servo PWM resolution
